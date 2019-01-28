d28a0aa582166a478e8c643711419253266ec468
archy <chinaarchy@gmail.com>
2019-01-28 14:42:25 +0800

    1.28 counting_sort

---
+++ 07_sorts/counting_sort.py
@@ -0,0 +1,36 @@
+from typing import List
+import itertools
+
+
+def counting_sort(a: List[int]):
+    if len(a) <= 1:
+        return
+
+    counts = [0] * (max(a) + 1)
+    for num in a:
+        counts[num] += 1
+    # 进行计数处理，累计求和
+    counts = list(itertools.accumulate(counts))
+
+    # 临时数组，存放临时排序结果
+    a_sorted = [0] * len(a)
+    for num in reversed(a):
+        index = counts[num] - 1
+        a_sorted[index] = num
+        counts[num] -= 1
+
+    a[:] = a_sorted
+
+
+if __name__ == "__main__":
+    a1 = [1, 2, 3, 4]
+    counting_sort(a1)
+    print(a1)
+
+    a2 = [1, 1, 1, 1]
+    counting_sort(a2)
+    print(a2)
+
+    a3 = [4, 5, 0, 9, 3, 3, 1, 9, 8, 7]
+    counting_sort(a3)
+    print(a3)
