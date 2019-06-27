c64168932399fd3231bd07f14c107fd0c51459db
archy <chinaarchy@gmail.com>
2019-06-27 11:23:30 +0800

    腾讯50题：206两种方法

--- Tencent-leetcode/206-reverseList.py
+++ Tencent-leetcode/206-reverseList.py
@@ -3,10 +3,16 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
-
+# 迭代方法
+# class Solution:
+#     def reverseList(self, head: ListNode) -> ListNode:
+#         cur, prev = head, None
+#         while cur:
+#             cur.next, cur, prev = prev, cur.next, cur
+#         return prev
+# 递归方法
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        cur, prev = head, None
-        while cur:
-            cur.next, cur, prev = prev, cur.next, cur
-        return prev
+    def reverseList(self, head: ListNode, tail=None) -> ListNode:
+        if head:
+            head.next, tail, head = tail, head, head.next
+        return self.reverseList(head, tail) if head else tail
