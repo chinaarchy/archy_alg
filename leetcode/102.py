c6d7877ba5e504141a2019a0ec18f51a48995ab1
archy <chinaarchy@gmail.com>
2019-07-10 14:09:52 +0800

    腾讯50题：104

---
+++ leetcode/102.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+import collections
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root: return []
+        result = []
+        queue = collections.deque()
+        queue.append(root)
+
+        while queue:
+            level_size = len(queue)
+            current_level = []
+
+            for _ in range(level_size):
+                node = queue.popleft()
+                current_level.append(node.val)
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+            result.append(current_level)
+        return result
---
+++ leetcode/22.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        self.list = []
+        self._gen(0, 0, n, "")
+        return self.list
+
+    def _gen(self, left, right, n, result):
+        if left == n and right == n:
+            self.list.append(result)
+            return
+        if left < n:
+            self._gen(left + 1, right, n, result + '(')
+        if left > right and right < n:
+            self._gen(left, right + 1, n, result + ')')
